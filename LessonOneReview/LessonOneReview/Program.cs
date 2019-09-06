9c75c592c33920a9673feffcc6a3113e700796f0
Devin Muhs <devinmuhs@gmail.com>
2019-09-06 19:07:37 -0400

    lesson one review

---
+++ LessonOneReview/LessonOneReview/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LessonOneReview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a random phrase");
+            string chars = Console.ReadLine();
+            char[] characters = chars.ToCharArray();
+            for (int i = 0; i < characters.Length; i++)
+                Console.Write(" " + characters[i]);
+        }
+    }
+}
